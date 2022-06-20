b8f63f6a75f27ea25df86a4d643a22f9ab15fc1b
Rohan Chandra <rohan.chandra@developersinstitute.co.nz>
2022-06-20 15:30:58 +1200

    npm install of icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
+// import UilReact from "@iconscout/react-unicons/icons/uil-react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
